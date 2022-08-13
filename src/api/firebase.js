8c3875f336ef15d885558bae4f8e2099d2df6ec8
Cristian Otero <cristianoteroadsi@gmail.com>
2022-08-13 10:37:17 -0500

    SetUp Proyect

---
+++ src/api/firebase.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCGtIuvAGwbLXvA-Hx_1m1rZlYcuBF0Dpk',
+  authDomain: 'auth-redux-88246.firebaseapp.com',
+  projectId: 'auth-redux-88246',
+  storageBucket: 'auth-redux-88246.appspot.com',
+  messagingSenderId: '595290453620',
+  appId: '1:595290453620:web:88da64a2ccdcfe04a68321',
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const auth = firebaseApp.auth()
