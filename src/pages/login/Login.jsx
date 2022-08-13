e4c3531761eab4d6032f5146d2e7fcc89f86d08e
Cristian Otero <cristianoteroadsi@gmail.com>
2022-08-13 13:28:26 -0500

    Completed login layout

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import './Login.scss';
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login">
+      <div className="login__content">
+        <div className="login__content-form">
+          <div className="title">
+            <h2>Welcome Back!</h2>
+            <p>We are glad to see you again, let's start! </p>
+          </div>
 
-export default Login
+          <form>
+            <div className="input-group">
+              <input type="email" required placeholder='you-email@example.com' />
+              <input type="password" required placeholder='Personal Key'/>
+            </div>
+
+            <button type="button">Log in</button>
+          </form>
+        </div>
+
+        <div className="login__content-banner">
+          <p>
+          Stay connected anywhere!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
