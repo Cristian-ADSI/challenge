618ce3a27e1521bd63046452ab025707bbb25863
Cristian Otero <cristianoteroadsi@gmail.com>
2022-08-15 20:00:43 -0500

    Responsive Navbar

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,6 +1,5 @@
 export const navLinks = [
   { index: 0, name: 'inicio', route: '/app' },
-  { index: 1, name: 'mi aprendizaje', route: '/app/category' },
+  { index: 1, name: 'curso', route: '/app/category' },
   { index: 2, name: 'lista de deseos', route: '/app' },
-  { index: 3, name: 'populares', route: '/app' },
 ];
